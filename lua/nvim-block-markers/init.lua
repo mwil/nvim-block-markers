b63f88e4e02a95f6d81e54ea0f9a3268b7e3878d
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2022-10-07 23:48:15 +0900

    Improved formatting.

--- lua/nvim-block-markers/init.lua
+++ lua/nvim-block-markers/init.lua
@@ -4,22 +4,26 @@
 -- https://www.youtube.com/watch?v=86sgKa0jeO4&ab_channel=s1n7ax
 --
 -- Clear everything again in the current buffer
+local api = vim.api
+local fn = vim.fn
+local ts = vim.treesitter
+
 local M = {}
 
 function M:enable_block_markers()
     self:refresh_block_markers()
 
-    vim.api.nvim_create_autocmd({"InsertLeave"},
-                                {callback = M.refresh_block_markers, pattern = {"*.py"}})
+    api.nvim_create_autocmd({"InsertLeave"},
+        {callback = M.refresh_block_markers, pattern = {"*.py"}})
 end
 
 function M:add_block_markers()
-    local ns_id = vim.api.nvim_create_namespace("bmark")
+    local ns_id = api.nvim_create_namespace("bmark")
 
     local language = "python"
-    local bufnr = vim.fn.bufnr("%")
+    local bufnr = fn.bufnr("%")
 
-    local language_tree = vim.treesitter.get_parser(bufnr, language)
+    local language_tree = ts.get_parser(bufnr, language)
     local syntax_tree = language_tree:parse()
     local root = syntax_tree[1]:root()
 
@@ -31,11 +35,10 @@ function M:add_block_markers()
     }
 
     for _, params in pairs(params_t) do
-        local query = vim.treesitter.parse_query(language,
-                                                 string.format(query_template, params.target))
+        local query = ts.parse_query(language, string.format(query_template, params.target))
 
         for _, _, metadata in query:iter_matches(root, bufnr) do
-            line_num = metadata[1].range[1] - 1
+            local line_num = metadata[1].range[1] - 1
 
             -- make sure there is no text on that line already
             if #vim.filetype.getlines(bufnr, line_num + 1) == 0 then
@@ -47,25 +50,27 @@ function M:add_block_markers()
                 }
 
                 -- Add virtual line: https://jdhao.github.io/2021/09/09/nvim_use_virtual_text/
-                local mark_id = vim.api.nvim_buf_set_extmark(0, ns_id, line_num, 0, opts)
+                local mark_id = api.nvim_buf_set_extmark(0, ns_id, line_num, 0, opts)
             end
         end
     end
 end
 
-function M:clear_block_markers(ns_id) vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1) end
+function M:clear_block_markers(ns_id)
+    api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+end
 
 function M:refresh_block_markers()
-    local ns_id = vim.api.nvim_create_namespace("bmark")
+    local ns_id = api.nvim_create_namespace("bmark")
 
     M:clear_block_markers(ns_id)
     M:add_block_markers()
 end
 
 function M:disable_block_markers()
-    local ns_id = vim.api.nvim_create_namespace("bmark")
+    local ns_id = api.nvim_create_namespace("bmark")
 
-    if #vim.api.nvim_buf_get_extmarks(0, ns_id, 0, -1, {}) > 0 then
+    if #api.nvim_buf_get_extmarks(0, ns_id, 0, -1, {}) > 0 then
         self:clear_block_markers(ns_id)
 
         return true
@@ -77,7 +82,9 @@ end
 function M:toggle_block_markers()
     local disabled = self:disable_block_markers()
 
-    if not disabled then self:enable_block_markers() end
+    if not disabled then
+        self:enable_block_markers()
+    end
 end
 
 return M
